870feafa64d8cae40285fc7ed10c776835c921e5
Raison Dsouza <raisondsouza@gmail.com>
2012-12-04 22:33:46 +0530

    upgraded rspec and added specs

--- spec/omniauth/strategies/instagram_spec.rb
+++ spec/omniauth/strategies/instagram_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::Instagram do
-  it 'should do some testing' do
-    pending
+  context "client options" do
+    subject do
+      OmniAuth::Strategies::Instagram.new({})
+    end
+
+    it "should have the correct site" do
+      expect(subject.options.client_options.site).to eq('https://api.instagram.com')
+    end
+
+    it "should have the correct authorize url" do
+      expect(subject.options.client_options.authorize_url).to eq('https://api.instagram.com/oauth/authorize')
+    end
+
+    it "should have the correct token url" do
+      expect(subject.options.client_options.token_url).to eq('https://api.instagram.com/oauth/access_token')
+    end
   end
 end
